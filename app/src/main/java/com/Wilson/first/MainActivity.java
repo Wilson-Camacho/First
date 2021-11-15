79af9b614fc0b6bc89aa085c82f122b4e26be4ec
Wilson-Camacho <wilsonc.act@gmail.com>
2021-11-15 13:13:27 +0100

    datos nuevos 11/11/2021

--- app/src/main/java/com/Wilson/first/MainActivity.java
+++ app/src/main/java/com/Wilson/first/MainActivity.java
@@ -1,8 +1,19 @@
 package com.Wilson.first;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +21,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView presionado =  (TextView) findViewById(R.id.textPress);
+        registerForContextMenu(presionado);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //getMenuInflater().inflate(R.menu.menu_context, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item1:
+                /*
+                Toast toast = Toast.makeText(this, "item copied", Toast.LENGTH_LONG);
+                toast.show();
+                */
+
+                final ConstraintLayout lay = findViewById(R.id.myMainActivityConstraint);
+                Snackbar snackbar = Snackbar
+                        .make(lay, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG);
+
+                /*OPCIONAL, PARA PODER QUITAR EL SNACKBAR
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(lay, "Action is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                 */
+                snackbar.show();
+
+                return true;
+
+            case R.id.item2:
+                Toast toast2 = (Toast) Toast.makeText(getApplicationContext(), "going context se", Toast.LENGTH_LONG);
+                toast2.show();
+
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+
+
+
 
     }
 }
